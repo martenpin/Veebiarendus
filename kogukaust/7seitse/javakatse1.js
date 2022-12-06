9340bf0f20a9ab32fddcc8c414bb4d7b9323c566
martenpin <110425032+martenpin@users.noreply.github.com>
2022-12-06 11:20:05 +0200

    pilt juurde jhava

--- kogukaust/7seitse/javakatse1.js
+++ kogukaust/7seitse/javakatse1.js
@@ -1,17 +1,21 @@
 let kasutajanimi = ""
 while (kasutajanimi == "") {
-    kasutajanimi = prompt ("mis on sinu nimi?" )
+    kasutajanimi = prompt("mis on sinu nimi?")
+}
+
+let pikknimi = "sul on pikk nimi " + kasutajanimi
+
+let lyhikenimi = "<h1>" + "sul on lühike nimi " + kasutajanimi + "</h1>"
+
+if (kasutajanimi.length > 6) {
+    kasutajanimi = pikknimi
+}
+
+if (kasutajanimi.length <= 6) {
+    kasutajanimi = lyhikenimi + "<img src=./assets/4a1df021749a54fc2d50ad03c65e25ba.jpg><img>"
 }
 
 let nimeelement = document.getElementById("nimi")
 
-    if (kasutajanimi.length <= 6) {
-        nimi= "sul on lühike nimi " + kasutajanimi
-        
-    }
-
-    if (kasutajanimi.length > 6) {
-        nimi= "sul on pikk nimi " + kasutajanimi
-    }
-
-    nimeelement.innerHTML = nimi
+nimeelement.innerHTML = kasutajanimi
+console.log(nimi)
