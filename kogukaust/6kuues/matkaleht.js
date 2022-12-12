a6305dd3681677a81bdd2c195f004c9e67f300b8
martenpin <110425032+martenpin@users.noreply.github.com>
2022-12-12 18:10:11 +0200

    kodutyy pooleli

---
+++ kogukaust/6kuues/matkaleht.js
@@ -0,0 +1,11 @@
+let buttonClickKontankt = "andmed"
+
+function buttonClickKontankt() {
+    let inputName = document.querySelector(sisendnimi).value
+    let inputmail = document.querySelector(sisendmail).value
+    let inputmessage = document.querySelector(sisends6num).value
+    console.log(inputName)
+    console.log(inputmail)
+    console.log(inputmessage)
+
+}
