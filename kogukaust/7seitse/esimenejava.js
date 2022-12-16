c467bba665fae308e8c745f76a450dfd2fc271ad
martenpin <110425032+martenpin@users.noreply.github.com>
2022-12-16 14:20:21 +0200

    yes

--- kogukaust/7seitse/esimenejava.js
+++ kogukaust/7seitse/esimenejava.js
@@ -1,23 +1,2 @@
-let kasutajanimi = ""
-while (kasutajanimi == "") {
-    kasutajanimi = prompt("mis on sinu nimi?")
-}
-
-let tervitus = "hommik <strong>" + kasutajanimi + "</strong>"
-let tervituselement = document.getElementById("tervitus")
-
-if (kasutajanimi == "Madis") {
-    tervitus = "hommik mu sõber " + kasutajanimi
-}
-
-tervitus = tervitus + "<h2>Tähed sinu nimes</h2>"
-
-
-
-for (let index = 0; index < kasutajanimi.length; index = index +1) {
-   tervitus += "<div> täht nr " + index + " on " + kasutajanimi[index] + "</div>"
-    
-}
-
-tervituselement.innerHTML = tervitus
-console.log(tervitus)
+let tervitusElement = document.getElementById("tervitus")
+tervitusElement.innerHTML = "jou jou"
