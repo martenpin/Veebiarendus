c534b3625c1836635bb129786fcf77eb017a5e49
martenpin <110425032+martenpin@users.noreply.github.com>
2022-12-19 10:24:29 +0200

    paus

--- kogukaust/10kümnes/todo.js
+++ kogukaust/10kümnes/todo.js
@@ -1,8 +1,17 @@
-const sisuElement = document.querySelector("sisu")
-
-
-
-
-function (params) {
-    
+function sisestayl() {
+    let sisestatudTekst = document.getElementById(tegevus)
+    let valjundElement = valjund
+    let valjund = sisestatudTekst
 }
+
+
+
+
+let tegevusElement = document.getElementById()
+let tegevustearv = 0;
+
+if (tegevustearv == tegevustearv) {
+let tegevustearv = tegevustearv + 1
+
+}
+
--- kogukaust/7seitse/esimenejava.js
+++ kogukaust/7seitse/esimenejava.js
@@ -1,2 +1,28 @@
+let kasutajanimi = prompt()
+
+// käsklus kus uus muutuja ehk variable, omistab parameetri lehel, id'lt HTMList "document.getElementById".
 let tervitusElement = document.getElementById("tervitus")
-tervitusElement.innerHTML = "jou jou"
+
+  tervitus = "jou " + kasutajanimi
+
+if (kasutajanimi.length == 0) {
+    tervitus ="Unustasid sisestada nime"    
+}
+
+if (kasutajanimi.length >= 5) {
+    tervitus = "jou jou " + kasutajanimi
+}
+
+// htmliga seondatud variableile omistatakse väärtus lõpuga ".innerHTML"
+tervitusElement.innerHTML = tervitus
+
+// alusta nr 1; kui muutuja on väiksem või võrdne 10ga ; lisa üks väärtus 
+
+for (let muutuja = 1; muutuja <= 10; muutuja++) {
+    console.log(muutuja)    
+}
+
+for (let tegija = 0; tegija < väärtused.length; tegija++) {
+    const element = array tegija];
+    
+}
