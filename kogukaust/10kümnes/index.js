48975f3d99dea39baf91ae15014b5046ef196108
martenpin <110425032+martenpin@users.noreply.github.com>
2022-12-25 09:49:31 +0200

    jk

--- kogukaust/10kümnes/index.js
+++ kogukaust/10kümnes/index.js
@@ -1,6 +1,16 @@
 const express = require("express")
 const app = express()
+
+function tervitus2 (req, res) {
+    res.end("<h1>Tere k6ik see pere, ytleb luukere<h1>")
+}
+
+
+
 app.get("/", function(req, res){
     res.end("Tere maailm")
 })
+
+app.get("/tervitus2", tervitus2)
+
 app.listen(3000)
